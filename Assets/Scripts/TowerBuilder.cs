1e714bfd51bcea41e659b79b3598fd5881d55012
Andre0y <Andreysirotkin2009@gmail.com>
2023-09-23 20:10:41 +0300

    Add platforms, beam, scripts

---
+++ Assets/Scripts/TowerBuilder.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class TowerBuilder : MonoBehaviour
+{
+    [SerializeField] private float _levelCount;
+    [SerializeField] private GameObject _beam;
+    [SerializeField] private SpawnPlatform _spawnPlatform;
+    [SerializeField] private FinishPlatform _finishPlatform;
+
+    [SerializeField] private Platform[] _platforms;
+
+    private void Start()
+    {
+        Build();
+    }
+
+    private void Build()
+    {
+        GameObject beam = Instantiate(_beam, transform);
+        beam.transform.localScale = new Vector3(1, _levelCount / 2, 1);
+
+        Vector3 spawnPosition = beam.transform.position;
+        spawnPosition.y += beam.transform.localScale.y;
+        
+        for (int i = 0; i < _levelCount; i++)
+        {
+            SpawnPlatform(_platforms[Random.Range(0, _platforms.Length)], ref spawnPosition, beam.transform);
+        }
+    }
+
+    private void SpawnPlatform(Platform platform, ref Vector3 spawnPosition, Transform parent)
+    {
+        Instantiate(platform, spawnPosition, Quaternion.Euler(0, Random.Range(0, 360), 0), parent);
+        spawnPosition.y -= 1;
+    }
+
+}
